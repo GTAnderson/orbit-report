05e29b83e69a504e2dcf790d0c848bf304900533
Garrett Anderson <garrett@Garretts-MBP.lan1>
2020-09-24 01:38:08 -0500

    shouldShowWarning

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -4,6 +4,7 @@ export class Satellite {
     type: string;
     operational: boolean;
     launchDate: string;
+     
     constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean){
         this.name = name;
         this.type = type;
@@ -11,4 +12,11 @@ export class Satellite {
         this.orbitType = orbitType;
         this.operational = operational;
     }
+
+    shouldShowWarning() {
+        if (this.type === 'Space Debris') {
+            return true;
+        }
+    }
+
 }
